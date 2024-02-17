48b63173fc3e5f5e33085665d79fa14027203cb9
NajibaTagougui <157955212+NajibaTagougui@users.noreply.github.com>
2024-02-17 14:53:33 +0100

    Update MainActivity.java
    J'ai mentionné les corrections à faire dans le fichier java

--- app/src/main/java/com/example/hellotoast/MainActivity.java
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -5,14 +5,17 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    //vous n'avez pas déclaré les variables
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // vous n'avez pas findViewByID pour les lier une variable de type TextView à votre TextView 
     }
 
     public void showToast (View view) {
+        //context est automatiquement généré ce n'est pas vous qui va l'écrire 
         Toast.makeText( context this, text: "Button Toast Clicked", Toast.LENGTH_SHORT).show();
     }
     usage
